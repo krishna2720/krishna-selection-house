364a139d1efee636ddd1da68f654dad0ea9019a5
krishna agarwal <krishna.125766@gmail.com>
2026-09-25 18:36:55 +0530

    see no of customers who visited your website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Home from "./pages/Home";
-
+import { Analytics } from "@vercel/analytics/react";
 // This site is a single page (no routing library needed), so App.jsx
 // simply renders the Home page. Keeping this file tiny makes it obvious
 // where to look if the project ever grows to need more pages.
 function App() {
-  return <Home />;
+
+    <>
+      <Home />
+      <Analytics /> {/* 2. Yahan component ke saath add karein */}
+    </>
 }
 
 export default App;
